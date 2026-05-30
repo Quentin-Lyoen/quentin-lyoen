cf67bfeadc216869ce3e577afb2a9f136683b76e
quentin.lyoen.etu <quentin.lyoen.etu@univ-lille.fr>
2026-05-30 17:44:54 +0200

    feat: implement home page component with introductory bio and styling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NavigationBarComponent } from "../../components/navigation-bar/navigation-bar.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [NavigationBarComponent],
+    imports: [],
 })
 export class HomeComponent {
 
