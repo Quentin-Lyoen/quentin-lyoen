600721303b7dadc662aea8491b788cb592a869ef
quentin.lyoen.etu <quentin.lyoen.etu@univ-lille.fr>
2026-06-23 14:54:25 +0200

    feat: implement degree page and update application routes with skills and degree components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SkillsComponent } from './pages/skills/skills.component';
+import { DegreeComponent } from './pages/degree/degree.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'skills', component: SkillsComponent },
+    { path: 'degree', component: DegreeComponent },
 ];
