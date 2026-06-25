ba291f8929266109ab8b084e1be17aff861fd1f8
quentin.lyoen.etu <quentin.lyoen.etu@univ-lille.fr>
2026-06-25 15:25:55 +0200

    feat: add portfolio-perso page containing personal project experience cards

--- src/app/pages/experience/pages/portfolio-perso/portfolio-perso.component.ts
+++ src/app/pages/experience/pages/portfolio-perso/portfolio-perso.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { ExperienceCardComponent } from "../experience-card/experience-card.component";
 
 @Component({
     selector: 'app-portfolio-perso',
     templateUrl: './portfolio-perso.component.html',
     styleUrls: ['portfolio-perso.component.scss'],
+    imports: [ExperienceCardComponent],
 })
 export class PortfolioPersoComponent {
     
